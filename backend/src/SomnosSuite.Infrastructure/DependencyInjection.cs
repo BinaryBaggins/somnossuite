73a6ee49c09ce51ffc866ffb11e71671ac88de3f
BinaryBaggins <80835556+BinaryBaggins@users.noreply.github.com>
2026-09-17 13:28:54 +0200

    Add local SQL Server development setup

--- backend/src/SomnosSuite.Infrastructure/DependencyInjection.cs
+++ backend/src/SomnosSuite.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using SomnosSuite.Application.Abstractions;
+using SomnosSuite.Infrastructure.Time;
 
 namespace SomnosSuite.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services)
         {
-            var currentAssembly = typeof(DependencyInjection).Assembly;
+            services.AddSingleton<IClock, SystemClock>();
 
             return services;
         }
